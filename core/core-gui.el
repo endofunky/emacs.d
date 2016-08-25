8eefbffc8382853e3212ab43b8c37b74a4e756a7
Tobias Svensson <tob@tobiassvensson.co.uk>
2016-08-25 22:47:17 +0100

    Use window-setup-hook

--- core/core-gui.el
+++ core/core-gui.el
@@ -2,6 +2,8 @@
   (setq frame-title-format '(multiple-frames "%b" "%b"))
   (set-default-font "Inconsolata for Powerline-11")
   (global-set-key (kbd "M-RET") 'toggle-frame-fullscreen)
-  (toggle-frame-maximized))
+
+  (setq frame-resize-pixelwise t)
+  (add-hook 'window-setup-hook 'toggle-frame-maximized))
 
 (provide 'core-gui)
