771bb49ee5d68c05948d7d70d662a44e153ad66f
Tobias Svensson <tob@tobiassvensson.co.uk>
2016-08-26 21:24:37 +0100

    Fix ido-ubiquitous

--- pkg/pkg-ido.el
+++ pkg/pkg-ido.el
@@ -1,20 +1,18 @@
 (use-package ido
   :ensure t
-  :defer t
-  :init
-  (evil-define-key 'normal global-map ",s" 'ido-switch-buffer)
-  :commands (ido-switch-buffer
-             ido-find-file
-             imenu)
   :config
+  (evil-define-key 'normal global-map ",s" 'ido-switch-buffer)
+
   (ido-mode 1)
   (ido-everywhere 1)
+
   (setq ido-enable-flex-matching t)
   (setq ido-create-new-buffer 'always)
   (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
   (setq ido-vertical-show-count t)
   (setq ido-case-fold t)
   (setq ido-use-faces t)
+
   (add-to-list 'ido-ignore-files "\\.DS_Store")
   (add-to-list 'ido-ignore-directories "\\.cache")
   (add-to-list 'ido-ignore-directories "\\.git")
@@ -24,27 +22,27 @@
   (add-to-list 'ido-ignore-directories "elpa")
   (add-to-list 'ido-ignore-directories "target")
 
-  (use-package ido-vertical-mode
+  (use-package smex
     :ensure t
+    :bind (("M-x" . smex)
+           ("M-X" . smex-major-mode-commands))
     :config
-    (ido-vertical-mode 1))
+    (smex-initialize))
 
   (use-package ido-ubiquitous
     :ensure t
     :config
     (ido-ubiquitous-mode 1))
 
+  (use-package ido-vertical-mode
+    :ensure t
+    :config
+    (ido-vertical-mode 1))
+
   (use-package flx-ido
     :ensure t
     :config
     (flx-ido-mode 1)
     (setq flx-ido-threshold 2000)))
-
-(use-package smex
-  :ensure t
-  :bind (("M-x" . smex)
-         ("M-X" . smex-major-mode-commands))
-  :config
-  (smex-initialize))
 
 (provide 'pkg-ido)
