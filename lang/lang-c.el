40e51f9955efcdd9200cc0e809f431eb067a5738
Tobias Svensson <tob@tobiassvensson.co.uk>
2016-08-28 14:22:58 +0100

    Add ac-c-headers

--- lang/lang-c.el
+++ lang/lang-c.el
@@ -4,7 +4,15 @@
   (add-to-list 'auto-mode-alist '("\\.h$" . c-mode))
   :config
   (setq c-default-style "k&r")
-  (setq c-basic-offset 4))
+  (setq c-basic-offset 4)
+
+  (use-package ac-c-headers
+    :ensure t
+    :init
+    (add-hook 'c-mode-hook
+              (lambda ()
+                (add-to-list 'ac-sources 'ac-source-c-headers)
+                (add-to-list 'ac-sources 'ac-source-c-header-symbols t)))))
 
 (use-package cmake-mode
   :ensure t
