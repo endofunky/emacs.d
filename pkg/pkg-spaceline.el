6f5499fcda0b5421179d412626b09f109477baab
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-03-06 12:14:02 +0000

    No longer use spaceline

--- pkg/pkg-spaceline.el
+++
@@ -1,8 +0,0 @@
-(use-package spaceline
-  :ensure t
-  :config
-  (require 'spaceline-config)
-  (spaceline-spacemacs-theme)
-  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state))
-
-(provide 'pkg-spaceline)
