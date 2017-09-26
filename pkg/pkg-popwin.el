03ee6a65eca416a0d4f9a91eedeaccae7c5560b8
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-09-26 11:53:05 +0100

    Move popwin REPL macro into pkg file

--- pkg/pkg-popwin.el
+++ pkg/pkg-popwin.el
@@ -3,6 +3,27 @@
   :config
   (popwin-mode 1)
 
+  (defmacro ts/define-repl (name buf fn)
+    "Define a popwin REPL wrapper function"
+    `(defun ,name ()
+       (interactive)
+       (let ((buffer-instance (get-buffer ,buf))
+             (options '(,buf
+                        :position bottom
+                        :height .4
+                        :stick t
+                        :tail t
+                        :noselect nil
+                        :dedicated t))
+             (buf (current-buffer)))
+         (cond ((null buffer-instance)
+                (funcall ,fn)
+                (switch-to-buffer buf)
+                (apply 'popwin:popup-buffer options))
+               ((get-buffer-window ,buf t)
+                (popwin:close-popup-window t))
+               (t (apply 'popwin:popup-buffer options))))))
+
   (add-to-list 'popwin:special-display-config
                '("*ag search*"
                  :dedicated t
