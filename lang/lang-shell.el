903e32aae6d087d03275b69c12e9057411faf643
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-09-26 15:26:34 +0100

    Add ,r binding for sh-mode

--- lang/lang-shell.el
+++ lang/lang-shell.el
@@ -8,7 +8,10 @@
          ("zprofile\\'" . sh-mode)
          ("zshenv\\'" . sh-mode)
          ("zshrc\\'" . sh-mode))
+  :init
+  (ts/define-repl ts/repl-zsh "*ansi-term*" #'(lambda () (ansi-term (getenv "SHELL"))))
   :config
+  (evil-define-key 'normal sh-mode-map ",r" 'ts/repl-zsh)
   (setq sh-basic-offset 2)
   (setq sh-indent-for-case-label 0)
   (setq sh-indent-for-case-alt '+))
