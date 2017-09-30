9e1cce09db4829887dd94f0d1dacb02e346833fd
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-09-30 12:53:46 +0100

    Bind C-f and RET in company map

--- pkg/pkg-company.el
+++ pkg/pkg-company.el
@@ -6,26 +6,28 @@
     (and (company-tooltip-visible-p)
          (company-explicit-action-p)))
 
-  (setq company-auto-complete #'ts/company-visible-and-explicit-action-p)
-
-  (setq company-minimum-prefix-length 2)
-  (setq company-idle-delay 0.05)
-  (setq company-selection-wrap-around t)
-  (setq company-tooltip-align-annotations t)
-  (setq company-tooltip-flip-when-above t)
-  (setq company-tooltip-margin 2)
-  (setq company-echo-delay 0)
-  (setq company-tooltip-limit 12)
-  (setq company-require-match 'never)
-
-  (setq company-frontends
-        '(company-pseudo-tooltip-frontend
-          company-echo-metadata-frontend))
+  (setq company-auto-complete #'ts/company-visible-and-explicit-action-p
+        company-minimum-prefix-length 2
+        company-idle-delay 0.05
+        company-dabbrev-ignore-case nil
+        company-dabbrev-downcase nil
+        company-selection-wrap-around t
+        company-tooltip-align-annotations t
+        company-tooltip-flip-when-above t
+        company-tooltip-margin 2
+        company-echo-delay 0
+        company-tooltip-limit 12
+        company-require-match 'never
+        company-frontends '(company-pseudo-tooltip-frontend
+                            company-echo-metadata-frontend))
 
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
   (define-key company-active-map (kbd "C-p") #'company-select-previous)
+  (define-key company-active-map (kbd "C-f") 'company-complete-selection)
+  (define-key company-active-map [return] 'company-complete-selection)
+  (define-key company-active-map (kbd "RET") 'company-complete-selection)
   (define-key company-active-map [tab]
     'company-select-next-if-tooltip-visible-or-complete-selection)
   (define-key company-active-map (kbd "TAB")
