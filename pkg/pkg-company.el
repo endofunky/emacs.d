24d94fc8b02ff078b11bb2427df041aa48825fa3
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-10-22 11:03:06 +0100

    Bring back custom TAB behaviour for company-mode

--- pkg/pkg-company.el
+++ pkg/pkg-company.el
@@ -1,6 +1,7 @@
 (use-package company
   :diminish company-mode
   :ensure t
+  :defer 1
   :config
   (defun ts/company-visible-and-explicit-action-p ()
     (and (company-tooltip-visible-p)
@@ -8,11 +9,13 @@
 
   (setq company-auto-complete #'ts/company-visible-and-explicit-action-p
         company-global-modes '(not message-mode help-mode)
+        company-begin-commands '(self-insert-command)
         company-minimum-prefix-length 2
-        company-idle-delay 0.1
+        company-idle-delay 0.3
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil
         company-selection-wrap-around t
+        company-show-numbers t
         company-tooltip-align-annotations t
         company-tooltip-flip-when-above t
         company-tooltip-margin 2
@@ -20,8 +23,30 @@
         company-require-match 'never
         company-frontends '(company-pseudo-tooltip-frontend))
 
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
+  (defun ts/check-expansion ()
+    (save-excursion
+      (if (looking-at "\\_>") t
+        (backward-char 1)
+        (if (looking-at "\\.") t
+          (backward-char 1)
+          (if (looking-at "->") t nil)))))
+
+  (defun ts/tab-indent-or-complete ()
+    (interactive)
+    (if (minibufferp)
+        (minibuffer-complete)
+      (if (ts/check-expansion)
+          (company-complete-common)
+        (indent-for-tab-command))))
+
+  (defun ts/evil-complete-lambda (arg)
+    "Ignores passed in arg like a lambda and runs company-complete"
+    (company-complete))
+
+  (setq evil-complete-next-func 'ts/evil-complete-lambda)
+  (setq evil-complete-previous-func 'ts/evil-complete-lambda)
+
+  (define-key evil-insert-state-map (kbd "TAB") 'ts/tab-indent-or-complete)
   (define-key company-active-map (kbd "C-n") #'company-select-next-or-abort)
   (define-key company-active-map (kbd "C-p") #'company-select-previous-or-abort)
   (define-key company-active-map (kbd "C-f") 'company-complete-selection)
