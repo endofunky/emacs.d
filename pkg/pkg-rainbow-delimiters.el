1411e7abce569b1e2286a9ba47737b32ccc547c7
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-10-24 20:13:36 +0100

    Only load rainbow delimiters in prog-mode

--- pkg/pkg-rainbow-delimiters.el
+++ pkg/pkg-rainbow-delimiters.el
@@ -1,8 +1,6 @@
 (use-package rainbow-delimiters
   :ensure t
   :config
-  (define-globalized-minor-mode global-rainbow-delimiters-mode
-    rainbow-delimiters-mode rainbow-delimiters-mode)
-  (global-rainbow-delimiters-mode t))
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (provide 'pkg-rainbow-delimiters)
