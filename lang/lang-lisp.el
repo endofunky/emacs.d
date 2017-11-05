7952407036b7f230b2f7a1f298fd248727e97e75
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-11-05 12:15:07 +0000

    Bind ,cc to emacs-lisp-compile-and-load

--- lang/lang-lisp.el
+++ lang/lang-lisp.el
@@ -56,10 +56,12 @@
   (evil-define-key 'normal emacs-lisp-mode-map ",ee" 'eval-expression)
   (evil-define-key 'visual emacs-lisp-mode-map ",er" 'eval-region)
   (evil-define-key 'normal emacs-lisp-mode-map ",r" 'ef-repl-ielm)
+  (evil-define-key 'normal emacs-lisp-mode-map ",cc" 'emacs-lisp-byte-compile-and-load)
   (evil-define-key 'normal lisp-interaction-mode-map ",eb" 'eval-buffer)
   (evil-define-key 'normal lisp-interaction-mode-map ",ee" 'eval-expression)
   (evil-define-key 'visual lisp-interaction-mode-map ",er" 'eval-region)
   (evil-define-key 'normal lisp-interaction-mode-map ",r" 'ef-repl-ielm)
+  (evil-define-key 'normal lisp-interaction-mode-map ",cc" 'emacs-lisp-byte-compile-and-load)
   (evil-define-key 'normal ielm-map ",r" 'ef-repl-ielm)
 
   (defun ef-emacs-lisp-recompile ()
