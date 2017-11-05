b4df9ab5acaf846ac38a3334157afebbcbef20f7
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-11-05 13:41:22 +0000

    Use smerge-command-prefix instead of evil-define-key

--- pkg/pkg-smerge.el
+++ pkg/pkg-smerge.el
@@ -1,10 +1,17 @@
 (use-package smerge-mode
-  :after evil
-  :defer t
+  :ensure t
   :commands smerge-mode
-  :config
-  (evil-define-key 'normal smerge-mode-map ",sc" 'smerge-keep-current)
-  (evil-define-key 'normal smerge-mode-map ",sn" 'smerge-next)
-  (evil-define-key 'normal smerge-mode-map ",sp" 'smerge-previous))
+  :init
+  (setq smerge-command-prefix (kbd "C-s"))
+
+  (defun ef-enable-smerge-maybe ()
+    "Auto-enable `smerge-mode' when merge conflict is detected."
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward "^<<<<<<< " nil :noerror)
+        (smerge-mode t))))
+
+  (add-hook 'find-file-hook 'ef-enable-smerge-maybe t)
+  (add-hook 'after-revert-hook 'sm-try-smerge t))
 
 (provide 'pkg-smerge)
