956d9f7cc688c269e9991dc74d7db4409ec4bfcc
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-11-05 14:13:43 +0000

    Add ef-toggle-scratch

--- pkg/pkg-evil.el
+++ pkg/pkg-evil.el
@@ -78,6 +78,28 @@
   (define-key evil-motion-state-map "K" nil)
   (define-key evil-normal-state-map "K" nil)
 
+  (defvar ef-toggle-scratch--prev-buffer nil)
+
+  (defun ef-toggle-scratch--goto-scratch ()
+    (if-let* ((scratch-buffer (get-buffer "*scratch*")))
+        (progn
+          (setq ef-toggle-scratch--prev-buffer (current-buffer))
+          (switch-to-buffer scratch-buffer))
+      (message "No *scratch* buffer found.")))
+
+  (defun ef-toggle-scratch--goto-prev-buffer ()
+    (if (buffer-live-p ef-toggle-scratch--prev-buffer)
+        (switch-to-buffer ef-toggle-scratch--prev-buffer)
+      (message "No buffer to switch back to.")))
+
+  (defun ef-toggle-scratch ()
+    (interactive)
+    (if (equal (buffer-name) "*scratch*")
+        (ef-toggle-scratch--goto-prev-buffer)
+      (ef-toggle-scratch--goto-scratch)))
+
+  (define-key evil-normal-state-map ",S" 'ef-toggle-scratch)
+
   (evil-add-hjkl-bindings package-menu-mode-map 'emacs
     "H" 'package-menu-quick-help))
 
