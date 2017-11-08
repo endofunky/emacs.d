14664848dfee2358ef1278cb6f3348ab4575aab2
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-11-08 07:49:50 +0000

    Move eldoc mode config into lang-lisp

--- lang/lang-lisp.el
+++ lang/lang-lisp.el
@@ -113,4 +113,12 @@
   :defer t
   :commands package-lint-current-buffer)
 
+(use-package eldoc
+  :diminish eldoc-mode
+  :config
+  (setq eldoc-idle-delay 0.5)
+
+  ;; Eldoc massively slows down cursor movement. This advice fixes that.
+  (advice-add 'eldoc-pre-command-refresh-echo-area :override #'ignore))
+
 (provide 'lang-lisp)
--- pkg/pkg-eldoc.el
+++
@@ -1,9 +0,0 @@
-(use-package eldoc
-  :diminish eldoc-mode
-  :config
-  (setq eldoc-idle-delay 0.5)
-
-  ;; Eldoc massively slows down cursor movement. This advice fixes that.
-  (advice-add 'eldoc-pre-command-refresh-echo-area :override #'ignore))
-
-(provide 'pkg-eldoc)
