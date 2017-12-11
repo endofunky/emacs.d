3f988fe7fba6662a0b46e316ef6b11eb99fe101b
Tobias Svensson <tob@tobiassvensson.co.uk>
2017-12-11 08:07:56 +0000

    Tweak lang-go

--- lang/lang-go.el
+++ lang/lang-go.el
@@ -4,9 +4,22 @@
   :config
   (evil-define-key 'normal go-mode-map ",," 'godef-jump)
   (evil-define-key 'normal go-mode-map ",." 'pop-tag-mark)
-  (setq ac-delay 0.3)
+
   (sp-with-modes '(go-mode)
-    (sp-local-pair "{" nil :post-handlers '((ef-sp-create-newline-and-enter-sexp "RET")))))
+    (sp-local-pair "{" nil :post-handlers '((ef-sp-create-newline-and-enter-sexp "RET"))))
+
+  (ef-shackle '("*Gofmt Errors*" :align bottom :size .4 :popup t :select t)
+              '("*go-rename*" :align bottom :size .4 :popup t :select t))
+
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook #'gofmt-before-save)
+
+  (defun ef-go-mode-hook ()
+    (if (not (string-match "go" compile-command))
+        (set (make-local-variable 'compile-command)
+             "go build -v && go vet")))
+
+  (add-hook 'go-mode-hook 'ef-go-mode-hook))
 
 (use-package go-rename
   :after go-mode
@@ -14,7 +27,11 @@
 
 (use-package company-go
   :after go-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-go)
+  (setq company-go-show-annotation t
+        company-go-insert-arguments nil))
 
 (use-package go-eldoc
   :after go-mode
