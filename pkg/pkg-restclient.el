6ce410ce717def9f4a89058fdb44f0c6870e8f19
Tobias Svensson <tob@tobiassvensson.co.uk>
2018-12-05 09:48:43 +0000

    Add org-babel integration for restclient-mode

--- pkg/pkg-restclient.el
+++ pkg/pkg-restclient.el
@@ -5,4 +5,12 @@
   :config
   (ef-shackle '("*HTTP Response*" :align bottom :size .4 :popup t :select t)))
 
+(use-package ob-restclient
+  :after org
+  :ensure t
+  :config
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((restclient . t))))
+
 (provide 'pkg-restclient)
