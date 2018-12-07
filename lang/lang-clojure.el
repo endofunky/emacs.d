b05ba9dda579897e4754bcb63fd849b11560929a
Tobias Svensson <tob@tobiassvensson.co.uk>
2018-12-07 11:34:08 +0000

    Fix clojure keybinds

--- lang/lang-clojure.el
+++ lang/lang-clojure.el
@@ -8,25 +8,25 @@
 
 (use-package cider
   :ensure t
-  :after clojure-mode
   :config
   (setq cider-prompt-for-symbol nil)
+
   (add-hook 'cider-mode-hook #'eldoc-mode)
+
+  (ef-shackle '(cider-repl-mode :align bottom :size .4 :popup t :select t))
+  (ef-shackle '("*cider-error*" :align bottom :size .4 :popup t :select t))
+
   (evil-define-key 'normal cider-mode-map ",," 'cider-find-var)
   (evil-define-key 'normal cider-mode-map ",." 'cider-pop-back)
   (evil-define-key 'normal cider-mode-map ",eb" 'cider-eval-buffer)
   (evil-define-key 'normal cider-mode-map ",ef" 'cider-eval-file)
   (evil-define-key 'normal cider-mode-map ",ed" 'cider-eval-defun-at-point)
   (evil-define-key 'visual cider-mode-map ",er" 'cider-eval-region)
-
-  (defun ef-close-current-window ()
-    "Closes the current window"
-    (interactive)
-    (delete-window (get-buffer-window)))
-
-  (ef-shackle '(cider-repl-mode :align bottom :size .4 :popup t :select t))
   (evil-define-key 'normal cider-mode-map ",r" 'cider-switch-to-repl-buffer)
-  (evil-define-key 'normal cider-repl-mode-map ",r" 'ef-close-current-window)
+
+  (evil-define-key 'normal cider-stacktrace-mode-map "q" 'cider-popup-buffer-quit-function)
+
+  (evil-define-key 'normal cider-repl-mode-map ",r" 'quit-window)
   (evil-define-key 'insert cider-repl-mode-map (kbd "<up>") 'cider-repl-previous-input)
   (evil-define-key 'insert cider-repl-mode-map (kbd "<down>") 'cider-repl-next-input))
 
