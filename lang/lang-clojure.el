f8f5d79e9df111833b15778f799c347d5fab048f
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-01-03 11:24:57 +0000

    Make cider keybinds slightly more sensible

--- lang/lang-clojure.el
+++ lang/lang-clojure.el
@@ -6,9 +6,15 @@
   (setq clojure--prettify-symbols-alist
         '(("fn" . ?λ)))
 
+  (defun ef-cider-jack-in (params)
+    "Quit cider if running and jack in again"
+    (interactive "P")
+    (cider-quit)
+    (cider-jack-in params))
+
   (add-hook 'clojure-mode-hook 'evil-cleverparens-mode)
   (evil-define-key 'normal clojure-mode-map ",cjq" 'cider-quit)
-  (evil-define-key 'normal clojure-mode-map ",cji" 'cider-jack-in))
+  (evil-define-key 'normal clojure-mode-map ",cji" 'ef-cider-jack-in))
 
 (use-package cider
   :ensure t
@@ -21,7 +27,20 @@
   (add-hook 'cider-repl-mode-hook #'eldoc-mode)
 
   (ef-shackle '(cider-repl-mode :align bottom :size .4 :popup t :select t)
+              '("*cider-test-report*" :align bottom :size .4 :popup t :select t)
               '("*cider-error*" :align bottom :size .4 :popup t :select t))
+
+  (defun ef-cider-run-test ()
+    "Re-evaluate buffer and run test at point"
+    (interactive)
+    (cider-eval-buffer)
+    (cider-test-run-test))
+
+  (defun ef-cider-run-ns-tests ()
+    "Re-evaluate buffer and run tests for namespace"
+    (interactive)
+    (cider-eval-buffer)
+    (cider-test-run-ns-tests nil))
 
   (evil-define-key 'normal cider-mode-map ",," 'cider-find-var)
   (evil-define-key 'normal cider-mode-map ",." 'cider-pop-back)
@@ -31,6 +50,9 @@
   (evil-define-key 'visual cider-mode-map ",er" 'cider-eval-region)
   (evil-define-key 'normal cider-mode-map ",r" 'cider-switch-to-repl-buffer)
   (evil-define-key 'normal cider-mode-map ",ns" 'cider-repl-set-ns)
+  (evil-define-key 'normal cider-mode-map ",tt" 'ef-cider-run-ns-tests)
+  (evil-define-key 'normal cider-mode-map ",tp" 'ef-cider-run-test)
+  (evil-define-key 'normal cider-test-report-mode-map "q" 'cider-popup-buffer-quit-function)
   (evil-define-key 'normal cider-stacktrace-mode-map "q" 'cider-popup-buffer-quit-function)
 
   (evil-define-key 'normal cider-repl-mode-map ",r" 'quit-window)
