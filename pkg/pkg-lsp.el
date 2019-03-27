5aabd21f98ac6bc4a2909147d4235acb9d2b5314
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-03-27 09:39:56 +0000

    Remove company-capf in lsp enabled buffers

--- pkg/pkg-lsp.el
+++ pkg/pkg-lsp.el
@@ -5,13 +5,22 @@
   (lsp-prefer-flymake :none)
   (lsp-auto-guess-root t)
   (lsp-inhibit-message t)
+  ;; eldoc bug??
+  (lsp-eldoc-enable-hover nil)
+  (lsp-eldoc-enable-signature-help nil)
+  (lsp-eldoc-prefer-signature-help nil)
+  (lsp-enable-symbol-highlighting nil)
   :config
   (evil-define-key 'normal lsp-mode-map ",," 'lsp-find-definition)
-  (evil-define-key 'normal lsp-mode-map ",." 'pop-tag-mark))
+  (evil-define-key 'normal lsp-mode-map ",." 'pop-tag-mark)
+
+  (defun ef-lsp-mode-hook ()
+    (setq-local company-backends (remove 'company-capf company-backends)))
+
+  (add-hook 'lsp-mode-hook 'ef-lisp-mode-hook))
 
 (use-package company-lsp
   :commands company-lsp
-  :after lsp-mode
   :ensure t)
 
 (provide 'pkg-lsp)
