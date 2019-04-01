7d4e8c215f53c3b53f19537c3262d385e3da68ec
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-04-01 11:03:07 +0100

    Don't defer loading of projectile

--- pkg/pkg-projectile.el
+++ pkg/pkg-projectile.el
@@ -1,11 +1,9 @@
 (use-package projectile
   :ensure t
-  :commands (projectile-project-p
-	     projectile-switch-project
-             projectile-find-file
-             projectile-ag
-             projectile-reset-cached-project-root)
-  :init
+  :custom
+  (projectile-completion-system 'ido)
+  (projectile-mode-line-prefix " proj")
+  :config
   (defun ef-projectile-find-file ()
     (interactive)
     (if (projectile-project-p)
@@ -14,14 +12,11 @@
 
   (evil-define-key 'normal global-map ",f" 'ef-projectile-find-file)
   (evil-define-key 'normal global-map ",p" 'projectile-switch-project)
-  :config
+
   (projectile-global-mode t)
-  (setq projectile-completion-system 'ido)
+
   ;; Workaround for slow down caused by modeline updates of projectile
   ;; See: https://github.com/bbatsov/projectile/issues/1183
-  (setq projectile-mode-line
-        '(:eval (format " proj[%s]"
-                        (projectile-project-name))))
   (add-to-list 'projectile-globally-ignored-directories "elpa")
   (add-to-list 'projectile-globally-ignored-directories ".cache")
   (add-to-list 'projectile-globally-ignored-directories "node_modules")
