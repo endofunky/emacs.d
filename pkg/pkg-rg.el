791c15f706abbac6e7789cce7cefa93d9429b959
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-04-02 12:45:24 +0100

    Set xref markers when searching

--- pkg/pkg-rg.el
+++ pkg/pkg-rg.el
@@ -4,6 +4,14 @@
   :init
   (define-key evil-normal-state-map ",/" #'deadgrep)
   :config
-  (setq deadgrep-project-root-function #'projectile-project-root))
+  (setq deadgrep-project-root-function #'projectile-project-root)
+
+  (defadvice deadgrep (before my activate)
+    (xref-push-marker-stack))
+
+  (defadvice deadgrep-visit-result (before my activate)
+    (xref-push-marker-stack))
+
+  (evil-define-key 'normal deadgrep-mode-map ",." #'pop-tag-mark))
 
 (provide 'pkg-rg)
