3311ab440a6e1ff60aaf97b5ea31196e7eaefac9
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-04-16 12:42:46 +0100

    Make transient play nicely with shackle

--- pkg/pkg-magit.el
+++ pkg/pkg-magit.el
@@ -9,7 +9,6 @@
   :diminish auto-revert-mode
   :custom
   (magit-completing-read-function 'magit-ido-completing-read)
-  (magit-display-buffer-function #'display-buffer)
   :init
   (defun ef-magit-diff-head ()
     "Execute `magit-diff' against current HEAD."
@@ -24,7 +23,28 @@
   (define-key evil-normal-state-map ",gb" 'magit-blame)
   (define-key evil-normal-state-map ",gc" 'magit-checkout)
   :config
+  (ef-shackle '(magit-diff-mode :align right :size .5 :popup t :select nil))
+
   (setenv "GIT_PAGER" ""))
+
+(use-package transient
+  :defer t
+  :ensure t
+  :config
+  (transient-bind-q-to-quit)
+
+  (defadvice transient-setup (before transient-setup activate)
+    (ef-transient-suspend-shackle-mode))
+
+  (defun ef-transient-suspend-shackle-mode ()
+    (when (bound-and-true-p shackle-mode)
+      (shackle-mode -1)
+      (add-hook 'post-transient-hook 'ef-transient-resume-shackle-mode)))
+
+  (defun ef-transient-resume-shackle-mode ()
+    (unless transient--prefix
+      (shackle-mode t)
+      (remove-hook 'post-transient-hook 'ef-transient-resume-shackle-mode))))
 
 (use-package evil-magit
   :after (evil magit)
@@ -35,8 +55,6 @@
   ;; http://whattheemacsd.com/setup-magit.el-01.html
   (defadvice magit-status (around magit-fullscreen activate)
     (window-configuration-to-register :magit-fullscreen)
-    (if (fboundp 'shackle-mode)
-        (shackle-mode -1))
     ad-do-it
     (delete-other-windows))
 
@@ -44,9 +62,7 @@
     "Restores the previous window configuration and kills the magit buffer"
     (interactive)
     (kill-buffer)
-    (jump-to-register :magit-fullscreen)
-    (if (fboundp 'shackle-mode)
-        (shackle-mode t)))
+    (jump-to-register :magit-fullscreen))
 
   (evil-define-key 'normal magit-status-mode-map "q" #'ef-magit-quit-session)
   (evil-define-key 'visual magit-status-mode-map "q" #'ef-magit-quit-session))
