05f5d0760f9aa15aea45679d49f6c22d72a79138
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-05-17 11:00:07 +0100

    More subdued header-line style

--- pkg/pkg-theme.el
+++ pkg/pkg-theme.el
@@ -19,6 +19,8 @@
      nil '(("\\<\\(FIXME\\|BUG\\|XXX\\|TODO\\|NOCOMMIT\\)\\>"
             1 '((:foreground "#cc6666") (:weight bold)) t))))
 
+
+  (set-face-attribute 'header-line nil :foreground (ef-color :base0E) :background (ef-color :base00))
   (set-face-attribute 'font-lock-doc-face nil :foreground (ef-color :base03))
   (set-face-attribute 'fringe nil :background (ef-color :base00))
   (set-face-attribute 'vertical-border nil :foreground (ef-color :base01)))
