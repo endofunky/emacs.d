42961caed8a30b2c96f10bac2edf8a215c3e92c0
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-10 08:43:02 +0100

    Bind common keys

--- pkg/pkg-exwm.el
+++ pkg/pkg-exwm.el
@@ -7,7 +7,41 @@
   (require 'exwm-config)
   (exwm-config-default)
 
-  (add-hook 'exwm-manage-finish-hook (lambda () (call-interactively #'exwm-input-release-keyboard)))
+  (defmacro ef-exwm-shell-command (key cmd)
+    (let ((fn `(lambda ()
+                 (interactive)
+                 (start-process-shell-command ,(car (split-string cmd)) nil ,cmd))))
+      (if (stringp key)
+          `(define-key evil-normal-state-map ,key ,fn)
+        `(exwm-input-set-key ,key ,fn))))
+
+  (defun ef-exwm-workspace-next ()
+    (interactive)
+    (when (< (exwm-workspace--position exwm-workspace--current)
+             (- (exwm-workspace--count) 1))
+      (ef-flycheck-close-window)
+      (exwm-workspace-switch
+       (+ (exwm-workspace--position exwm-workspace--current) 1))))
+
+  (defun ef-exwm-workspace-prev ()
+    (interactive)
+    (when (> (exwm-workspace--position exwm-workspace--current) 0)
+      (ef-flycheck-close-window)
+      (exwm-workspace-switch
+       (- (exwm-workspace--position exwm-workspace--current) 1))))
+
+  (exwm-input-set-key (kbd "C-M-l") #'ef-exwm-workspace-next)
+  (exwm-input-set-key (kbd "C-M-h") #'ef-exwm-workspace-prev)
+
+  (ef-exwm-shell-command ",eL" "i3lock -e -c 000000 --nofork")
+  (ef-exwm-shell-command ",ef" "google-chrome-stable")
+  (ef-exwm-shell-command ",es" "spotify")
+  (ef-exwm-shell-command ",ex" "xterm")
+  (ef-exwm-shell-command (kbd "<print>") "scrot -e 'mv $f ~/media/images/'")
+  (ef-exwm-shell-command (kbd "M-<print>") "scrot -s -e 'mv $f ~/media/images/'")
+
+  (ef-add-hook exwm-manage-finish-hook
+    (call-interactively #'exwm-input-release-keyboard))
 
   (defadvice exwm-input-grab-keyboard (after ef activate)
     (evil-normal-state))
@@ -17,22 +51,42 @@
 
   (evil-define-key 'normal exwm-mode-map (kbd "i") 'exwm-input-release-keyboard)
   (push ?\i exwm-input-prefix-keys)
-  (push ?\, exwm-input-prefix-keys)
-  (exwm-init)
-  (display-time-mode t)
+  (exwm-init))
+
+(use-package battery
+  :after exwm
+  :custom
+  (battery-mode-line-format "⚡ %b%p%% ")
+  :config
   (display-battery-mode t))
 
+(use-package time
+  :after exwm
+  :custom
+  (display-time-default-load-average nil)
+  :config
+  (display-time-mode t))
+
 (use-package pulseaudio-control
+  :after exwm
+  :ensure t
   :commands (pulseaudio-control-increase-volume
              pulseaudio-control-decrease-volume
              pulseaudio-control-toggle-current-source-mute
              pulseaudio-control-toggle-current-sink-mute)
-  :after exwm
-  :ensure t
   :init
   (exwm-input-set-key (kbd "<XF86AudioMute>") #'pulseaudio-control-toggle-current-sink-mute)
   (exwm-input-set-key (kbd "<XF86AudioMicMute>") #'pulseaudio-control-toggle-current-source-mute)
   (exwm-input-set-key (kbd "<XF86AudioLowerVolume>") #'pulseaudio-control-decrease-volume)
   (exwm-input-set-key (kbd "<XF86AudioRaiseVolume>") #'pulseaudio-control-increase-volume))
+
+(use-package backlight
+  :ensure t
+  :after exwm
+  :commands (backlight-inc
+             backlight-dev)
+  :config
+  (exwm-input-set-key (kbd "<XF86MonBrightnessUp>") #'backlight-inc)
+  (exwm-input-set-key (kbd "<XF86MonBrightnessDown>") #'backlight-dec))
 
 (provide 'pkg-exwm)
