c5a0f4065651f1c86ec508515e8b781cb8ca8781
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-12 07:38:13 +0100

    Improve flycheck-error-list handling

--- pkg/pkg-flycheck.el
+++ pkg/pkg-flycheck.el
@@ -1,11 +1,11 @@
-(require 'use-package)
-(require 'core-lib)
 (require 'pkg-shackle)
 
 (use-package flycheck
   :ensure t
   :custom
-  (flycheck-check-syntax-automatically '(save mode-enabled))
+  (flycheck-check-syntax-automatically '(save mode-enabled idle-buffer-switch))
+  (flycheck-buffer-switch-check-intermediate-buffers t)
+  (flycheck-idle-buffer-switch-delay 0.01)
   (flycheck-disabled-checkers '(ruby-reek emacs-lisp-checkdoc))
   (flycheck-display-errors-delay 0.1)
   (flycheck-emacs-lisp-initialize-packages 'auto)
@@ -24,50 +24,27 @@
   (global-flycheck-mode t)
 
   (defvar ef-flycheck-may-toggle t)
-  (defvar ef-flycheck-last-file-buffer nil)
 
-  (defun ef-flycheck-close-window ()
-    (when ef-flycheck-may-toggle
-      (when-let ((buf (get-buffer flycheck-error-list-buffer)))
-        (if-let ((win (get-buffer-window buf)))
-            (delete-window win))
-        (kill-buffer buf))))
-
-  (defun ef-flycheck-open-window ()
-    (if ef-flycheck-may-toggle
-        (flycheck-list-errors)))
-
-  (defun ef-flycheck-toggle-window-maybe ()
-    (if (and ef-flycheck-may-toggle flycheck-current-errors)
-        (ef-flycheck-open-window)
-      (ef-flycheck-close-window)))
-
-  (ef-add-hook (post-command-hook after-change-major-mode-hook)
+  (ef-add-hook (post-command-hook
+                buffer-list-update-hook
+                after-change-major-mode-hook)
     :fn ef-flycheck-post-command-hook
-    (let ((current (current-buffer)))
-      (cond ((and (not buffer-file-name)
-                  (not (minibufferp current))
-                  (not (string= flycheck-error-list-buffer
-                                (buffer-name current))))
-             (setq ef-flycheck-last-file-buffer nil)
-             (ef-flycheck-close-window))
-            ((and buffer-file-name
-                  (not (eq major-mode 'exwm-mode))
-                  (not (minibufferp current))
-                  (not (eq ef-flycheck-last-file-buffer
-                           current))
-                  ef-flycheck-may-toggle)
-             (setq ef-flycheck-last-file-buffer current)
-             (ef-flycheck-toggle-window-maybe)))))
+    (when (and ef-flycheck-may-toggle
+               (not flycheck-current-errors)
+               (not (minibufferp (current-buffer)))
+               (get-buffer flycheck-error-list-buffer))
+      (delete-windows-on flycheck-error-list-buffer)))
+
+  (ef-add-hook (flycheck-before-syntax-check-hook
+                flycheck-after-syntax-check-hook)
+    (when flycheck-current-errors
+      (flycheck-list-errors)))
 
   (ef-add-hook minibuffer-setup-hook :fn ef-flycheck-minibuffer-setup-hook
     (setq ef-flycheck-may-toggle nil))
 
   (ef-add-hook minibuffer-exit-hook :fn ef-flycheck-minibuffer-exit-hook
-    (setq ef-flycheck-may-toggle t))
-
-  (ef-add-hook flycheck-after-syntax-check-hook
-    (ef-flycheck-toggle-window-maybe)))
+    (setq ef-flycheck-may-toggle t)))
 
 (use-package pkg-info
   :after flycheck
