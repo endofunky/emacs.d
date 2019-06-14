045cf4a846b0851949898c80a87114377f32a59a
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-14 14:40:22 +0100

    Hide mode line in write-mode

--- pkg/pkg-write.el
+++ pkg/pkg-write.el
@@ -1,9 +1,13 @@
+(use-package hide-mode-line
+  :ensure t)
+
 (use-package writegood-mode
   :ensure t)
 
 (define-derived-mode write-mode
   text-mode "Write"
   "Major mode for writing."
+  (hide-mode-line-mode t)
   (flyspell-mode t)
   (writegood-mode t)
   (visual-line-mode t))
