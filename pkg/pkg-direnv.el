6f200224f8daa112b683d26161a996e2966d2a72
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-06-18 17:33:39 +0100

    Run direnv on eshell directory changes

--- pkg/pkg-direnv.el
+++ pkg/pkg-direnv.el
@@ -1,6 +1,7 @@
 (use-package direnv
   :ensure t
   :config
-  (direnv-mode))
+  (direnv-mode)
+  (add-hook 'eshell-directory-change-hook #'direnv-update-directory-environment))
 
 (provide 'pkg-direnv)
