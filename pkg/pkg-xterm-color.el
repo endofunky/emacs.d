f4b1b3cdb27b869e97258f67b5188790e242cc6a
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-07-31 16:49:38 +0100

    Remove xterm-color

--- pkg/pkg-xterm-color.el
+++
@@ -1,9 +0,0 @@
-(use-package xterm-color
-  :ensure t
-  :config
-  (setenv "TERM" "xterm-256color")
-  (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-  (setq comint-output-filter-functions
-        (remove 'ansi-color-process-output comint-output-filter-functions)))
-
-(provide 'pkg-xterm-color)
