ff1ec1bd69d9ed83fcd57203943ccfc5bc413c74
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-07-31 07:29:55 +0100

    Remove intero-mode

--- lang/lang-haskell.el
+++ lang/lang-haskell.el
@@ -2,7 +2,7 @@
   :ensure t
   :mode "\\.hs\\'"
   :custom
-  (haskell-font-lock-symbols t)
+  (haskell-font-lock-symbols nil)
   :config
   (evil-define-key 'normal haskell-mode-map ",cc" 'haskell-compile))
 
@@ -13,19 +13,5 @@
 (use-package haskell-indent
   :after haskell-mode
   :hook (haskell-mode . turn-on-haskell-indent))
-
-(use-package intero
-  :ensure t
-  :commands intero-mode
-  :init
-  (add-hook 'haskell-mode-hook #'intero-mode)
-  :config
-  (ef-shackle '(intero-repl-mode :align below :size .4 :popup t :select t))
-
-  (evil-define-key 'visual intero-mode-map ",er" 'intero-repl-eval-region)
-  (evil-define-key 'normal intero-mode-map ",ef" 'intero-repl-load)
-  (evil-define-key 'normal intero-mode-map ",r" 'intero-repl)
-  (evil-define-key 'normal intero-mode-map ",," 'intero-goto-definition)
-  (evil-define-key 'normal intero-repl-mode-map ",r" 'quit-window))
 
 (provide 'lang-haskell)
