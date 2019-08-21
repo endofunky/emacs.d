86109485c4be5841d4b35cb294e22675cf528c6a
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-08-21 15:34:39 +0100

    Always update direnv before running lsp

--- pkg/pkg-direnv.el
+++ pkg/pkg-direnv.el
@@ -2,6 +2,9 @@
   :ensure t
   :config
   (direnv-mode)
+  (defadvice lsp (before ef-direnv activate)
+    (direnv-update-environment))
+
   (ef-add-hook eshell-directory-change-hook
     (direnv-update-directory-environment)
     (setq eshell-path-env (getenv "PATH"))))
