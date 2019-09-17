e57c8c6f9ac9e8297637ffde2e04e30639f21a5c
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-09-17 15:33:39 +0100

    Add more keybinds for alchemist

--- lang/lang-elixir.el
+++ lang/lang-elixir.el
@@ -13,12 +13,18 @@
   (add-hook 'elixir-mode-hook 'alchemist-mode)
   :config
   (ef-shackle
+   '("*alchemist help*" :align bottom :size .4 :popup t :select t)
+   '("*alchemist-refcard*" :align bottom :size .4 :popup t :select t)
    '("*alchemist test report*" :align bottom :size .4 :popup t :select t))
 
   (ef-define-repl ef-repl-alchemist "*Alchemist-IEx*" 'alchemist-iex-project-run)
+  (evil-define-key 'normal alchemist-mode-map ",m" 'alchemist-help)
+  (evil-define-key 'normal alchemist-mode-map ",cc" 'alchemist-compile)
+  (evil-define-key 'normal alchemist-mode-map ",eb" 'alchemist-iex-compile-this-buffer-and-go)
+  (evil-define-key 'visual alchemist-mode-map ",er" 'alchemist-iex-send-region)
   (evil-define-key 'normal alchemist-mode-map ",r" 'ef-repl-alchemist)
   (evil-define-key 'normal alchemist-mode-map ",tp" 'alchemist-mix-test-at-point)
-  (evil-define-key 'normal alchemist-mode-map ",tt" 'alchemist-mix-test-file)
+  (evil-define-key 'normal alchemist-mode-map ",tt" 'alchemist-project-run-tests-for-current-file)
   (evil-define-key 'normal alchemist-iex-mode-map ",r" 'ef-repl-alchemist))
 
 (use-package flycheck-mix
