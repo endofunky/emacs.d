a4dc71d81f7aae400b786522244ce9a4630b03cc
Tobias Svensson <tob@tobiassvensson.co.uk>
2019-09-17 15:22:44 +0100

    Use lsp-mode for Elixir

--- lang/lang-elixir.el
+++ lang/lang-elixir.el
@@ -2,7 +2,9 @@
   :mode (("\\.ex\\'" . elixir-mode)
          ("\\.exs\\'" . elixir-mode))
   :commands elixir-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-hook 'elixir-mode-hook #'lsp))
 
 (use-package alchemist
   :ensure t
@@ -10,10 +12,13 @@
   :init
   (add-hook 'elixir-mode-hook 'alchemist-mode)
   :config
+  (ef-shackle
+   '("*alchemist test report*" :align bottom :size .4 :popup t :select t))
+
   (ef-define-repl ef-repl-alchemist "*Alchemist-IEx*" 'alchemist-iex-project-run)
   (evil-define-key 'normal alchemist-mode-map ",r" 'ef-repl-alchemist)
-  (evil-define-key 'normal alchemist-iex-mode-map ",r" 'ef-repl-alchemist)
-  (evil-define-key 'normal alchemist-mode-map ",," 'alchemist-goto-definition-at-point)
-  (evil-define-key 'normal alchemist-mode-map ",." 'alchemist-goto-jump-back))
+  (evil-define-key 'normal alchemist-mode-map ",tp" 'alchemist-mix-test-at-point)
+  (evil-define-key 'normal alchemist-mode-map ",tt" 'alchemist-mix-test-file)
+  (evil-define-key 'normal alchemist-iex-mode-map ",r" 'ef-repl-alchemist))
 
 (provide 'lang-elixir)
