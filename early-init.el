10f09afb6ea81503b42c43acde73690d83d861d1
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-04-23 16:44:01 +0100

    Remove marmelade package archive

--- early-init.el
+++ early-init.el
@@ -27,7 +27,6 @@
               package-archives
               '(("melpa-stable" . "http://stable.melpa.org/packages/")
                 ("melpa"        . "http://melpa.org/packages/")
-                ("marmalade"    . "http://marmalade-repo.org/packages/")
                 ("org"          . "http://orgmode.org/elpa/")
                 ("gnu"          . "http://elpa.gnu.org/packages/"))
               site-run-file nil)
