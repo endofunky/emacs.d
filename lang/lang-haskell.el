4b46005cfed1bd0c0c9098aa6832c9a42c7d41fb
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-04-23 16:42:38 +0100

    Remove intero, which is now deprecated

--- lang/lang-haskell.el
+++ lang/lang-haskell.el
@@ -12,18 +12,4 @@
 (use-package haskell-doc
   :after haskell-mode)
 
-(use-package intero
-  :ensure t
-  :commands intero-mode
-  :init
-  (add-hook 'haskell-mode-hook #'intero-mode)
-  :config
-  (ef-shackle '(intero-repl-mode :align below :size .4 :popup t :select t))
-
-  (evil-define-key 'visual intero-mode-map ",er" 'intero-repl-eval-region)
-  (evil-define-key 'normal intero-mode-map ",ef" 'intero-repl-load)
-  (evil-define-key 'normal intero-mode-map ",r" 'intero-repl)
-  (evil-define-key 'normal intero-mode-map ",," 'intero-goto-definition)
-  (evil-define-key 'normal intero-repl-mode-map ",r" 'quit-window))
-
 (provide 'lang-haskell)
