08255f4092ecfafb57a5bb1a3c88ba6aa5c6f41b
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-04 10:44:18 +0100

    Remove eshell configuration

--- base/base-company.el
+++ base/base-company.el
@@ -2,7 +2,7 @@
   :ensure t
   :custom
   (company-auto-complete #'ef-company-visible-and-explicit-action-p)
-  (company-global-modes '(not message-mode help-mode eshell-mode))
+  (company-global-modes '(not message-mode help-mode))
   (company-begin-commands '(self-insert-command))
   (company-minimum-prefix-length 2)
   (company-idle-delay 0.3)
--- base/base-direnv.el
+++ base/base-direnv.el
@@ -1,12 +1,10 @@
+(require 'core-lib)
+
 (use-package direnv
   :ensure t
   :config
   (direnv-mode)
   (defadvice lsp (before ef-direnv activate)
-    (direnv-update-environment))
-
-  (ef-add-hook eshell-directory-change-hook
-    (direnv-update-directory-environment)
-    (setq eshell-path-env (getenv "PATH"))))
+    (direnv-update-environment)))
 
 (provide 'base-direnv)
