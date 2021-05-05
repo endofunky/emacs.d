4d64ddc3d4459233d1ff40e5df452606adddfd64
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-05 12:56:33 +0100

    Customize smartparens via :custom

--- base/base-smartparens.el
+++ base/base-smartparens.el
@@ -1,18 +1,15 @@
 (use-package smartparens
   :ensure t
+  :custom
+  (sp-autoskip-closing-pair 'always)
+  (sp-highlight-pair-overlay nil)
+  (sp-highlight-wrap-overlay nil)
+  (sp-highlight-wrap-tag-overlay nil)
+  (sp-show-pair-delay 0)
+  (sp-show-pair-from-inside t)
   :config
-  (require 'smartparens-config)
-
   (show-smartparens-global-mode -1)
   (smartparens-global-mode t)
-
-  (setq sp-autoescape-string-quote nil)
-  (setq sp-autoskip-closing-pair 'always)
-  (setq sp-highlight-pair-overlay nil)
-  (setq sp-highlight-wrap-overlay nil)
-  (setq sp-highlight-wrap-tag-overlay nil)
-  (setq sp-show-pair-delay 0)
-  (setq sp-show-pair-from-inside t)
 
   ;; Disable ' pairing where it makes sense.
   (sp-local-pair 'calc-mode "'" nil :actions nil)
@@ -26,11 +23,6 @@
   (sp-local-pair 'org-mode "'" nil :actions nil)
   (sp-local-pair 'tex-mode "'" nil :actions nil)
   (sp-local-pair 'text-mode "'" nil :actions nil)
-
-  ;; Enable smartparens in minibuffer
-  (setq sp-ignore-modes-list
-        (delete 'minibuffer-inactive-mode sp-ignore-modes-list))
-  (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
 
   (defun ef-sp-create-newline-and-enter-sexp (&rest _ignored)
     "Open a new brace or bracket expression, with relevant newlines and indent"
