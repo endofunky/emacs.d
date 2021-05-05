8c21988c01f1de7e2754a71eac32b08640a5e635
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-05 20:18:59 +0100

    Move base-company to core-company

---
+++ core/core-company.el
@@ -0,0 +1,73 @@
+(require 'core-evil)
+
+(use-package company
+  :ensure t
+  :demand t
+  :custom
+  (company-auto-complete #'ef-company-visible-and-explicit-action-p)
+  (company-begin-commands '(self-insert-command))
+  (company-dabbrev-downcase nil)
+  (company-dabbrev-ignore-case nil)
+  (company-frontends '(company-pseudo-tooltip-frontend))
+  (company-global-modes '(not message-mode help-mode))
+  (company-idle-delay 0.3)
+  (company-minimum-prefix-length 2)
+  (company-require-match 'never)
+  (company-selection-wrap-around t)
+  (company-show-numbers t)
+  (company-tooltip-align-annotations t)
+  (company-tooltip-flip-when-above t)
+  (company-tooltip-limit 12)
+  (company-tooltip-margin 2)
+  (evil-complete-next-func 'ef-evil-complete-lambda)
+  (evil-complete-previous-func 'ef-evil-complete-lambda)
+  :general
+  (:states 'normal :keymaps 'company-active-map
+	   "<return>" 'company-complete-selection
+	   "<tab>" 'company-select-next-if-tooltip-visible-or-complete-selection
+	   "C-f" 'company-complete-selection
+	   "C-n" #'company-select-next-or-abort
+	   "C-p" #'company-select-previous-or-abort)
+  (:states 'insert
+	   "<tab>" 'ef-tab-indent-or-complete)
+  :functions (ef-check-expansion
+	      ef-minibuffer-completion-hook)
+  :config
+  (declare-function company-tooltip-visible-p "company")
+  (declare-function company-complete-common "company")
+  (declare-function company-explicit-action-p "company")
+
+  (defun ef-company-visible-and-explicit-action-p ()
+    (and (company-tooltip-visible-p)
+         (company-explicit-action-p)))
+
+  (defun ef-check-expansion ()
+    (save-excursion
+      (if (looking-at "\\_>") t
+        (backward-char 1)
+        (if (looking-at "\\.") t
+          (backward-char 1)
+          (if (looking-at "->") t nil)))))
+
+  (defun ef-tab-indent-or-complete ()
+    "Either indent or start company completion."
+    (interactive)
+    (if (minibufferp)
+        (minibuffer-complete)
+      (if (ef-check-expansion)
+          (company-complete-common)
+        (indent-for-tab-command))))
+
+  (defun ef-evil-complete-lambda (arg)
+    "Ignores passed in arg like a lambda and runs company-complete"
+    (company-complete))
+
+  (global-company-mode t))
+
+(use-package company-statistics
+  :ensure t
+  :after company
+  :config
+  (company-statistics-mode))
+
+(provide 'core-company)
