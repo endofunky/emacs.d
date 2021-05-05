8046617684567ef574a9d79a1a987bef7f204b36
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-05 20:48:22 +0100

    Move base-rainbow-delimeters to core-rainbow-delimiters

--- base/base-rainbow-delimiters.el
+++
@@ -1,8 +0,0 @@
-(use-package rainbow-delimiters
-  :ensure t
-  :config
-  ;; Only load rainbow-delimiters-mode in prog-mode so it doesn't
-  ;; break diffs in magit.
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
-
-(provide 'base-rainbow-delimiters)
