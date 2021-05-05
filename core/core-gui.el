17c983999c17cf2c1fc83065164aab0db777266f
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-05 10:02:07 +0100

    Use use-package :custom for exec-path-from-shell-variables

--- core/core-gui.el
+++ core/core-gui.el
@@ -51,15 +51,14 @@
            (null (getenv "TERM_PROGRAM")))
   :custom
   (exec-path-from-shell-check-startup-files nil)
+  (exec-path-from-shell-variables
+   (append exec-path-from-shell-variables
+           '("NIX_PATH"
+             "NIX_PROFILES"
+             "NIX_REMOTE"
+             "NIX_SSL_CERT_FILE"
+             "NIX_USER_PROFILE_DIR")))
   :config
-  (customize-set-variable 'exec-path-from-shell-variables
-                          (append exec-path-from-shell-variables
-                                  '("NIX_PATH"
-                                    "NIX_PROFILES"
-                                    "NIX_REMOTE"
-                                    "NIX_SSL_CERT_FILE"
-                                    "NIX_USER_PROFILE_DIR")))
-  (message "exec init")
   (exec-path-from-shell-initialize))
 
 (provide 'core-gui)
