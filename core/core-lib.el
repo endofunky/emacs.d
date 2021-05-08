373d2f761bc6559e54beb43d292d050b195517be
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-08 15:36:35 +0100

    Fix ef-split-list docstring

--- core/core-lib.el
+++ core/core-lib.el
@@ -182,11 +182,11 @@ appended."
 ;;
 
 (cl-defun ef-split-list (list parts &key (last-part-longer nil))
-  ;; Split LIST into PARTS parts.  They will all be the same
-  ;; length except the last one which will be shorter or, if
-  ;; LAST-PART-LONGER is true, longer.  Doesn't deal with the case
-  ;; where there are less than PARTS elements in LIST at all (it does
-  ;; something, but it may not be sensible).
+  "Split LIST into PARTS parts.  They will all be the same length
+except the last one which will be shorter or, if LAST-PART-LONGER
+is true, longer.  Doesn't deal with the case where there are less
+than PARTS elements in LIST at all (it does something, but it may
+not be sensible). "
   (cl-loop with size = (if last-part-longer
                            (floor (length list) parts)
                          (ceiling (length list) parts))
