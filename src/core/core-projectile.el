06cb81402b43c48bc4cdcb27a317ec53b70a46ce
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-15 14:00:39 +0100

    Fix projectile not picking up the no-litter directory

--- src/core/core-projectile.el
+++ src/core/core-projectile.el
@@ -5,6 +5,10 @@
   :custom
   (projectile-completion-system 'ivy)
   (projectile-mode-line-prefix " P")
+  (projectile-cache-file
+   (no-littering-expand-var-file-name "projectile/cache.el"))
+  (projectile-known-projects-file
+   (no-littering-expand-var-file-name "projectile/known-projects.el"))
   :commands (projectile-toggle-between-implementation-and-test)
   :general
   (:states 'normal :prefix ef-prefix
