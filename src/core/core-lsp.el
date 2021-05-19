a5b2ba0c8e98e31c8f67395076f27216c7c90ced
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-19 09:00:30 +0100

    Get lsp-mode to start up again

--- src/core/core-lsp.el
+++ src/core/core-lsp.el
@@ -9,9 +9,16 @@
   (lsp-eldoc-enable-hover t)
   (lsp-enable-snippet nil)
   (lsp-enable-symbol-highlighting nil)
-  (lsp-prefer-flymake :none)
+  (lsp-prefer-flymake nil)
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-gopls-experimental-staticcheck nil)
   :general
   (:states 'normal :keymap 'lsp-mode-map :prefix ef-prefix
-	   "," 'lsp-find-definition))
+	   "," 'lsp-find-definition)
+  :config
+  (ef-add-popup "*lsp-performance*" :ephemeral 5 :size 0.15)
+  (require 'lsp-mode)
+  (require 'lsp-modeline)
+  (require 'lsp-headerline))
 
 (provide 'core-lsp)
