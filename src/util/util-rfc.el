33dca6cb8033e87dd6d1da5a2653edacb92eae77
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-20 14:37:27 +0100

    Move RFC browser into help menu

--- src/util/util-rfc.el
+++ src/util/util-rfc.el
@@ -3,7 +3,8 @@
   :commands (rfc-mode-browse)
   :general
   (:states 'normal :prefix ef-prefix
-           "cdR" '(rfc-mode-browse :wk "Browse RFCs"))
+           "h"  '(nil :wk "Help")
+           "hR" '(rfc-mode-browse :wk "Browse RFCs"))
   :config
   (ef-add-popup 'rfc-mode :size 0.5))
 
