2ecd9a980fce392851a2a1e51a2cc45d0bfa1054
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-20 14:55:18 +0100

    Don't reset help menu map

--- src/util/util-rfc.el
+++ src/util/util-rfc.el
@@ -3,7 +3,6 @@
   :commands (rfc-mode-browse)
   :general
   (:states 'normal :prefix ef-prefix
-           "h"  '(nil :wk "Help")
            "hR" '(rfc-mode-browse :wk "Browse RFCs"))
   :config
   (ef-add-popup 'rfc-mode :size 0.5))
