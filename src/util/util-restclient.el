ab1e6b372d866e2b47bee001f7c60dd635c3c44b
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-05-20 12:18:18 +0100

    Add ,R keybind to toggle to restclient-mode scratch buffer

--- src/util/util-restclient.el
+++ src/util/util-restclient.el
@@ -1,12 +1,30 @@
 (require 'core-shackle)
 
+(defconst ef-restclient-buffer-name "*restclient*"
+  "Scratch buffer name for restclient-mode.")
+
 (use-package restclient
   :mode (("\\.rest\\'" . restclient-mode)
          ("\\.restclient\\'" . restclient-mode))
-  :commands (restclient-mode)
+  :commands (ef-restclient
+             restclient-mode)
   :ensure t
+  :general
+  (:states 'normal :prefix ef-prefix
+	   "R" '(ef-restclient :wk "Toggle RESTclient"))
   :config
-  (ef-add-popup "*HTTP Response*"))
+  (ef-add-popup "*HTTP Response*")
+  (defun ef-restclient ()
+    "Toggle scratch buffer for `restclient-mode'."
+    (interactive)
+    (if (string= ef-restclient-buffer-name
+                 (buffer-name (current-buffer)))
+        (bury-buffer)
+      (unless (get-buffer ef-restclient-buffer-name)
+        (with-current-buffer (generate-new-buffer ef-restclient-buffer-name)
+          (set-buffer-modified-p nil)
+          (restclient-mode t)))
+      (switch-to-buffer ef-restclient-buffer-name))))
 
 (use-package ob-restclient
   :after (restclient org)
