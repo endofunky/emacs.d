2c187cf4747c7afab540b67ecad3e3107a6466da
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-06-10 16:24:03 +0100

    Start cleaning up org-mode config

--- src/lang/lang-org.el
+++ src/lang/lang-org.el
@@ -6,80 +6,66 @@
   :ensure t
   :mode (("\\.\\(org\\|org_archive\\)$" . org-mode))
   :custom
-  (org-agenda-custom-commands
-   '(("n" "Agenda and all TODOs" ((agenda "") (alltodo ""))
-      ((org-agenda-span 'week)
-       (org-deadline-warning-days 0)
-       (org-agenda-skip-deadline-prewarning-if-scheduled t)
-       (org-agenda-skip-deadline-if-done t)
-       (org-agenda-skip-scheduled-if-deadline-is-shown t)
-       (org-agenda-todo-ignore-deadlines 'all)
-       (org-agenda-todo-ignore-scheduled 'all)))))
-  (org-agenda-files `(,(expand-file-name "~/Dropbox/org/")))
-  (org-agenda-restore-windows-after-quit nil)
-  (org-agenda-start-on-weekday 1)
-  (org-agenda-time-grid
-   '((daily today remove-match require-timed)
-     (600 800 1000 1200 1400 1600 1800 2000 2200)
-     "......" "----------------"))
-  (org-agenda-window-setup 'other-window)
-  (org-archive-location "%s_archive::datetree/* Archived Tasks")
+  (org-adapt-indentation nil)
   (org-confirm-babel-evaluate nil)
   (org-deadline-warning-days 7)
-  (org-default-notes-file (expand-file-name "~/Dropbox/org/notes.org"))
+  (org-edit-src-content-indentation 0)
   (org-enforce-todo-dependencies t)
   (org-fontify-quote-and-verse-blocks t)
   (org-hide-emphasis-markers t)
   (org-log-done 'time)
   (org-return-follows-link t)
   (org-src-fontify-natively t)
+  (org-src-preserve-indentation nil)
   (org-src-tab-acts-natively t)
-  :commands (org-agenda org-capture ef-org-agenda)
+  :commands (org-capture org-switchb)
   :hook
   (org-mode . flyspell-mode)
-  :init
-  (defun ef-org-notes ()
-    (interactive)
-    (find-file org-default-notes-file))
+  (org-mode . org-indent-mode)
+  (org-mode . turn-on-auto-fill)
   :general
   (:states 'normal :prefix ef-prefix
+   "," '(org-open-at-point :wk "Open Link")
+   "." '(org-mark-ring-goto :wk "Pop Back")
    "o" '(nil :wk "Org")
-   "oa" '(ef-org-agenda :wk "Agenda")
    "oc" '(org-capture :wk "Capture")
-   "oo" '(ef-org-notes :wk "Notes"))
+   "os" '(org-switchb :wk "Switch Buffer")
   (:states 'normal :prefix ef-prefix :keymaps 'org-mode-map
    "o" '(nil :wk "Org")
    "oc" '(org-toggle-checkbox :wk "Toggle Checkbox")
    "o," '(org-priority-up :wk "Priority Up")
    "o." '(org-priority-down :wk "Priority Down")
-   "oA" '(ef-org-archive-done-tasks :wk "Archive Done Tasks")
    "op" '(org-priority :wk "Cycle Priority")
-   "ot" '(org-todo :wk "Cycle TODO"))
+   "ot" '(org-todo :wk "Cycle TODO")
+   "l" '(nil :wk "Org Links")
+
+   "li" '(org-insert-link :wk "Insert Link")
+   "lr" '(org-roam-insert :wk "Insert Roam Link")
+   "lt" '(org-toggle-link-display :wk "Toggle Links")
+
+   "t" '(nil :wk "Org Table")
+   "t=" '(org-table-align :wk "Align")
+   "tH" '(org-table-move-column-left :wk "Move Column Left")
+   "tJ" '(org-table-move-row-down :wk "Move Row Down")
+   "tK" '(org-table-move-row-up :wk "Move Row Up")
+   "tL" '(org-table-move-column-right :wk "Move Column Right")
+   "td" '(org-table-delete-column :wk "Delete Columns")
+   "te" '(org-table-edit-field :wk "Edit Field")
+   "th" '(org-table-previous-field :wk "Previous Field")
+   "ti" '(nil :wk "Insert")
+   "ti-" '(org-table-insert-hline :wk "Insert Line")
+   "tic" '(org-table-insert-column :wk "Insert Column")
+   "tih" '(org-table-hline-and-move :wk "Insert Line and Move")
+   "tir" '(org-table-insert-row :wk "Insert Row")
+   "tl" '(org-table-next-field :wk "Next Field")
+   "ts" '(org-table-sort-lines :wk "Sort Rows")
+   "tt" '(org-table-create :wk "Create"))
   :config
   (require 'org-capture)
   (require 'org-install)
 
-  (setq org-capture-templates
-        (append org-capture-templates
-                '(("t" "Todo" entry (file+headline "~/Dropbox/org/notes.org" "Tasks")
-                   "** TODO %^{Task} %?"))))
-
   (defun org-switch-to-buffer-other-window (&rest args)
     (apply 'switch-to-buffer-other-window args))
-
-  (ef-keep-other-windows org-agenda)
-
-  (defun ef-org-agenda ()
-    (interactive)
-    (org-agenda nil "n"))
-
-  (defun ef-org-archive-done-tasks ()
-    (interactive)
-    (org-map-entries
-     (lambda ()
-       (org-archive-subtree)
-       (setq org-map-continue-from (outline-previous-heading)))
-     "/DONE" 'tree))
 
   (org-babel-do-load-languages
    'org-babel-load-languages
@@ -91,23 +77,50 @@
   (when window-system
     (define-key org-mode-map (kbd "M-RET") 'toggle-frame-fullscreen))
 
-  (ef-add-popup " *Agenda Commands*")
-  (ef-add-popup "*Org Select*"))
+  (ef-add-popup "*Org Select*")
 
-(use-package calendar
-  :defer t
-  :config
-  (ef-add-popup 'calendar-mode))
+  (defun org-cycle-hide-drawers (state)
+    "Re-hide all drawers after a visibility state change."
+    (when (and (derived-mode-p 'org-mode)
+               (not (memq state '(overview folded contents))))
+      (save-excursion
+        (let* ((globalp (memq state '(contents all)))
+               (beg (if globalp
+                        (point-min)
+                      (point)))
+               (end (if globalp
+                        (point-max)
+                      (if (eq state 'children)
+                          (save-excursion
+                            (outline-next-heading)
+                            (point))
+                        (org-end-of-subtree t)))))
+          (goto-char beg)
+          (while (re-search-forward org-drawer-regexp end t)
+            (save-excursion
+              (beginning-of-line 1)
+              (when (looking-at org-drawer-regexp)
+                (let* ((start (1- (match-beginning 0)))
+                       (limit
+                        (save-excursion
+                          (outline-next-heading)
+                          (point)))
+                       (msg (format
+                             (concat
+                              "org-cycle-hide-drawers:  "
+                              "`:END:`"
+                              " line missing at position %s")
+                             (1+ start))))
+                  (if (re-search-forward "^[ \t]*:END:" limit t)
+                      (outline-flag-region start (point-at-eol) t)
+                    (user-error msg)))))))))))
 
 (use-package evil-org
   :ensure t
   :after org
   :hook
   (org-mode . evil-org-mode)
-  (org-mode . evil-org-set-key-theme)
-  :custom
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
+  (org-mode . evil-org-set-key-theme))
 
 (use-package org-roam
   :ensure t
