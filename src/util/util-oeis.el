34dba2d1d0cef9cd92618971d99cba75704929e3
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-06-21 16:40:15 +0100

    Add util-oeis for searching OEIS database

---
+++ src/util/util-oeis.el
@@ -0,0 +1,8 @@
+(use-package oeis
+  :general
+  (:states 'normal :prefix ef-prefix
+   "ho" '(nil :wk "OEIS")
+   "hos" '(oeis-search :wk "OEIS Search")
+   "hob" '(oeis-browse-anum :wk "OEIS Browse ANUM")))
+
+(provide 'util-oeis)
