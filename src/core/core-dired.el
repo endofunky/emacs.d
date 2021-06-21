81bf180ff2c0daaf101d0ee5d46abb5fdf1faea6
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-06-21 13:46:00 +0100

    Add ef-dired-here helper and rebind ,d to it

--- src/core/core-dired.el
+++ src/core/core-dired.el
@@ -3,9 +3,18 @@
   :commands (dired)
   :general
   (:states 'normal :prefix ef-prefix
-   "d" 'dired)
+   "d" '(ef-dired-here :wk "dired Here")
+   "D" 'dired)
   (:states 'normal :keymaps 'dired-mode-map
    "q" 'kill-this-buffer)
+  (:states 'normal :prefix ef-prefix :keymaps 'dired-mode-map
+   "d" 'bury-buffer
+   "D" 'bury-buffer)
+  :init
+  (defun ef-dired-here ()
+    "Open dired buffer in current directory."
+    (interactive)
+    (dired "."))
   :custom
   (dired-auto-revert-buffer t)
   (dired-create-destination-dirs 'ask)
