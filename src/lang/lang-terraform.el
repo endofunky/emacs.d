4b3c777b304bf81bc01e0f720ec11386c4e2199c
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-06-22 10:54:26 +0100

    Add lang-terraform

---
+++ src/lang/lang-terraform.el
@@ -0,0 +1,34 @@
+(require 'core-projectile)
+
+(use-package terraform-mode
+  :ensure t
+  :mode (("\\.tf\\'" . terraform-mode))
+  :hook
+  (terraform-mode . ef-terraform-mode-enable-lsp)
+  :config
+  (defun ef-terraform-mode-enable-lsp ()
+    "Conditionally enable lsp-mode for terraform-mode projects."
+    (interactive)
+    (direnv-update-directory-environment)
+    (when (projectile-project-p)
+      (when (locate-file "terraform-ls" exec-path)
+        (setq-local lsp-terraform-server '("terraform-ls" "serve")))
+
+      (when (locate-file "terraform-lsp" exec-path)
+        (setq-local lsp-terraform-server "terraform-lsp"))
+
+      (lsp))))
+
+(use-package lsp-mode
+  :defer t
+  :config
+  (declare-function lsp-flycheck-add-mode "lsp-mode")
+
+  (lsp-flycheck-add-mode 'terraform-mode))
+
+(use-package company-terraform
+  :ensure t
+  :config
+  (company-terraform-init))
+
+(provide 'lang-terraform)
