77f5002cdf415399984e9fec4ecd7564b0ed2f1f
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-07-02 16:51:44 +0100

    Don't select flycheck-error-list-buffer

--- src/core/core-flycheck.el
+++ src/core/core-flycheck.el
@@ -36,7 +36,7 @@
                 (eq evil-state 'replace))
       ad-do-it))
 
-  (ef-add-popup flycheck-error-list-buffer :size .2)
+  (ef-add-popup flycheck-error-list-buffer :size .2 :select nil)
 
   (fset 'flycheck-may-use-echo-area-p 'ignore)
 
