8f211b3d17948cefec4bed74302a5b376242b88b
Tobias Svensson <tob@tobiassvensson.co.uk>
2021-07-19 11:22:22 +0100

    Remove oeis.el

--- src/util/util-oeis.el
+++
@@ -1,9 +0,0 @@
-(use-package oeis
-  :load-path "vendor/"
-  :general
-  (:states 'normal :prefix ef-prefix
-   "ho" '(nil :wk "OEIS")
-   "hos" '(oeis-search :wk "OEIS Search")
-   "hob" '(oeis-browse-anum :wk "OEIS Browse ANUM")))
-
-(provide 'util-oeis)
