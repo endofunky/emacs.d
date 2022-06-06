3cfa242be98e0351b94a02fe03ba868f78f76629
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-06 12:27:12 +0100

    Add lang-rust

---
+++ src/lang/lang-rust.el
@@ -0,0 +1,22 @@
+(require 'core-lib)
+(require 'core-shackle)
+
+(use-package rust-mode
+  :custom
+  (rust-format-on-save t)
+  :ensure t
+  :config
+  (ef-add-hook rust-mode-hook :interactive t
+    (direnv-update-directory-environment)
+
+    (if (locate-file "rls" exec-path exec-suffixes 1)
+        (lsp)))
+
+  (ef-add-popup "*rustfmt*"))
+
+(ef-deflang rust
+  :compile rust-compile
+  :compile-and-run rust-run
+  :test-all rust-test)
+
+(provide 'lang-rust)
