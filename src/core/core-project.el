ac3c11ad8e8883022edcc069bd49e28ec810b7d5
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-17 15:00:33 +0100

    Use vterm-mode when opening project shell

--- src/core/core-project.el
+++ src/core/core-project.el
@@ -11,7 +11,7 @@
    "p!" '(project-shell-command :wk "Run Shell Command in Project Root")
    "pf" '(project-find-file :wk "Find File in Project")
    "pF" '(project-forget-project :wk "Forget Project")
-   "ps" '(project-shell :wk "Open Shell")
+   "pS" '(ef-project-shell :wk "Open Project Shell")
    "pd" '(project-find-dir :wk "Open Dired in Project Directory")
    "pD" '(project-dired :wk "Open Dired in Project Root")
    "pv" '(project-vc-dir :wk "Run Vc-Dir in Project Root")
@@ -19,6 +19,14 @@
    "pp" '(ef-project-switch-project :wk "Switch Project")
    "ps" '(project-switch-to-buffer :wk "Switch to Project Buffer"))
   :config
+  (defun ef-project-shell ()
+    "Open VTerm Shell in Project Root"
+    (interactive)
+    (let ((root (project-root (project-current t))))
+      (let ((default-directory root)
+            (project-current-inhibit-prompt t))
+        (vterm))))
+
   (defun ef-project-switch-project (dir)
     "\"Switch\" to another project and find file."
     (interactive (list (project-prompt-project-dir)))
