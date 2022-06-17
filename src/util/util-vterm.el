da4b5640016d61757e1e892f9a8e0fb59b630bc7
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-17 22:46:43 +0100

    Bring back ef-vterm-popup-and-go
    WTF did I delete this?!

--- src/util/util-vterm.el
+++ src/util/util-vterm.el
@@ -19,7 +19,8 @@
    "M-P" 'ef-popup-switch-popup-buffer
    "M-p" 'ef-popup-toggle)
   (:states 'normal :prefix ef-prefix
-   "v" '(ef-vterm-popup :wk "VTerm Popup"))
+   "v" '(ef-vterm-popup :wk "VTerm Popup")
+   "V" '(ef-vterm-popup-and-go :wk "VTerm Popup & Go"))
   :config
   (declare-function vterm-send-string "vterm")
   (declare-function ef--vterm-sentinel "util-vterm")
@@ -38,6 +39,20 @@
     (if-let ((buf (get-buffer "*vterm-popup*")))
         (display-buffer "*vterm-popup*")
       (vterm "*vterm-popup*")))
+
+  (defun ef-vterm-popup-and-go ()
+    "Go to current file's directory in vterm popup."
+    (interactive)
+    (if-let* ((file (buffer-file-name (current-buffer)))
+              (dir (file-name-directory file)))
+        (if-let ((buf (get-buffer "*vterm-popup*")))
+            (progn
+              (display-buffer "*vterm-popup*")
+              (with-current-buffer buf
+                (vterm-send-string (concat "cd " dir))))
+          (vterm "*vterm-popup*"))
+
+      (message "Buffer is not visiting a file.")))
 
   (ef-add-popup "*vterm-popup*" :size 0.4)
 
