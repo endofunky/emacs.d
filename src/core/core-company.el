8d2a2e26de0c65e8a5389a3f9429fd50d702df77
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-18 14:31:35 +0100

    Start completing immediately as typing

--- src/core/core-company.el
+++ src/core/core-company.el
@@ -20,7 +20,7 @@
                               help-mode
                               gud-mode
                               vterm-mode))
-  (company-idle-delay 0.3)
+  (company-idle-delay 0.001)
   (company-minimum-prefix-length 2)
   (company-require-match 'never)
   (company-selection-wrap-around t)
