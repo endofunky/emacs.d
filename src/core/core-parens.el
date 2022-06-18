de98df2bc865d56a5d1d6492dcbb0aaa290521a3
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-18 18:11:17 +0100

    Set show-paren-delay to 0 again

--- src/core/core-parens.el
+++ src/core/core-parens.el
@@ -3,7 +3,7 @@
 (use-package paren
   :custom
   (show-paren-priority -50) ; Highlight parens in region
-  (show-paren-delay 0.1)
+  (show-paren-delay 0)
   (show-paren-when-point-inside-paren t)
   (show-paren-when-point-in-periphery t)
   :config
