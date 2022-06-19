8796d5c546743d7ac5a3fc43987eaa52f69a6469
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-19 15:51:10 +0100

    Add evil-textobj-tree-sitter

--- src/core/core-tree-sitter.el
+++ src/core/core-tree-sitter.el
@@ -18,4 +18,63 @@
   :config
   (global-tree-sitter-mode))
 
+(use-package evil-textobj-tree-sitter
+  :after (evil tree-sitter)
+  :functions (evil-textobj-tree-sitter-get-textobj
+               evil-textobj-tree-sitter-goto-textobj)
+  :config
+  (defvar evil-normal-state-map)
+  (defvar evil-inner-text-objects-map)
+  (defvar evil-outer-text-objects-map)
+
+  (define-key evil-outer-text-objects-map "B"
+    (evil-textobj-tree-sitter-get-textobj "block.outer"))
+  (define-key evil-inner-text-objects-map "B"
+    (evil-textobj-tree-sitter-get-textobj "block.inner"))
+
+  (define-key evil-outer-text-objects-map "F"
+    (evil-textobj-tree-sitter-get-textobj "function.outer"))
+  (define-key evil-inner-text-objects-map "F"
+    (evil-textobj-tree-sitter-get-textobj "function.inner"))
+
+  (define-key evil-outer-text-objects-map "C"
+    (evil-textobj-tree-sitter-get-textobj "class.outer"))
+  (define-key evil-inner-text-objects-map "C"
+    (evil-textobj-tree-sitter-get-textobj "class.inner"))
+
+  (define-key evil-outer-text-objects-map "S"
+    (evil-textobj-tree-sitter-get-textobj "statement.outer"))
+  (define-key evil-inner-text-objects-map "S"
+    (evil-textobj-tree-sitter-get-textobj "statement.inner"))
+
+  (define-key evil-outer-text-objects-map "A"
+    (evil-textobj-tree-sitter-get-textobj ("conditional.outer" "loop.outer")))
+
+  (define-key evil-inner-text-objects-map "A"
+    (evil-textobj-tree-sitter-get-textobj ("conditional.inner" "loop.inner")))
+
+  ;; Goto start of next function
+  (define-key evil-normal-state-map (kbd "]f")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer")))
+
+  ;; Goto start of previous function
+  (define-key evil-normal-state-map (kbd "[f")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer" t)))
+
+  ;; Goto end of next function
+  (define-key evil-normal-state-map (kbd "]F")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer" nil t)))
+
+  ;; Goto end of previous function
+  (define-key evil-normal-state-map (kbd "[F")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer" t t))))
+
 (provide 'core-tree-sitter)
