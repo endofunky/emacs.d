a8ac6d8cdc31ede6de4c96c7c8e70eed1792dfb1
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-20 16:16:59 +0100

    Add comment for eldoc-echo-area-use-multiline-p

--- src/lang/lang-elisp.el
+++ src/lang/lang-elisp.el
@@ -126,6 +126,7 @@ https://emacs.stackexchange.com/questions/10230/how-to-indent-keywords-aligned"
   :defer t
   :custom
   (eldoc-idle-delay 0.5)
+  ;; Limit eldoc documentation in echo area to 1 line.
   (eldoc-echo-area-use-multiline-p 1)
   :config
   ;; Eldoc massively slows down cursor movement. This advice fixes that.
