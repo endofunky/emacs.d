6ff03d72d0d8ddd6f676e455513c4a9244835028
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-20 22:22:32 +0100

    Copy eglot documentation lookup function from doom-emacs

--- src/core/core-lsp.el
+++ src/core/core-lsp.el
@@ -1,6 +1,5 @@
 (require 'core-direnv)
 (require 'core-shackle)
-(require 'eieio)
 
 (use-package eglot
   :custom
@@ -18,11 +17,14 @@
              eglot-code-action-organize-imports
              ef-enable-lsp-maybe)
   :defines (eglot-server-programs)
-  :functions (eglot-lsp-server)
+  :functions (eglot-lsp-server
+              eglot--hover-info
+              eglot--TextDocumentPositionParams
+              eglot--current-server-or-lose)
   :config
   (declare-function eglot--guess-contact "ext:eglot")
 
-  (ef-add-popup "^\\*eglot-help")
+  (ef-add-popup "^\\*eglot-help" :regexp t :size 0.4)
 
   (defun ef-lsp-organize-imports ()
     (interactive)
@@ -51,7 +53,38 @@
   (add-to-list 'eglot-server-programs
                '(ruby-mode . (eglot-solargraph "solargraph"
                                                "socket"
-                                               "--port" :autoport))))
+                                               "--port" :autoport)))
+
+  ;; The following is taken from doom-emacs' eglot.el:
+  ;; https://github.com/doomemacs/doomemacs/blob/master/modules/tools/lsp/autoload/eglot.el
+  ;;
+  ;; HACK Eglot removed `eglot-help-at-point' in joaotavora/eglot@a044dec for a
+  ;;      more problematic approach of deferred to eldoc.
+  (declare-function jsonrpc-request "ext:jsonrpc")
+
+  (defvar ef-eglot--help-buffer nil)
+
+  (defun ef-eglot-lookup-documentation (_identifier)
+    "Request documentation for the thing at point."
+    (interactive "P")
+    (eglot--dbind ((Hover) contents range)
+        (jsonrpc-request (eglot--current-server-or-lose) :textDocument/hover
+                         (eglot--TextDocumentPositionParams))
+      (let ((blurb (and (not (seq-empty-p contents))
+                        (eglot--hover-info contents range)))
+            (hint (thing-at-point 'symbol)))
+        (if blurb
+            (with-current-buffer
+                (or (and (buffer-live-p ef-eglot--help-buffer)
+                         ef-eglot--help-buffer)
+                    (setq ef-eglot--help-buffer
+                          (generate-new-buffer "*eglot-help*")))
+              (with-help-window (current-buffer)
+                (rename-buffer (format "*eglot-help for %s*" hint))
+                (with-current-buffer standard-output (insert blurb))
+                (setq-local nobreak-char-display nil)))
+          (display-local-help))))
+    'deferred))
 
 (use-package consult-eglot
   :after (eglot consult)
@@ -68,6 +101,8 @@
   :compile-backend-quit eglot-shutdown
   :compile-nav-jump xref-find-definitions
   :compile-nav-pop-back pop-tag-mark
+
+  :doc-point ef-eglot-lookup-documentation
 
   :refactor-imports ef-lsp-organize-imports
   :refactor-rename eglot-rename
