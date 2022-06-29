5b0afd786c258d359edf5b5c596dec0aebf20890
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-06-29 08:56:48 +0100

    Add GraphViz compilation keybinds

--- lisp/lang/lang-dot.el
+++ lisp/lang/lang-dot.el
@@ -5,6 +5,12 @@
   :mode (("\\.dot\\'" . graphviz-dot-mode)
          ("\\.gv\\'" . graphviz-dot-mode))
   :commands (graphviz-dot-mode)
+  :general
+  (:prefix ef-local-leader :states 'visual :keymaps 'graphviz-dot-mode-map
+   "c" '(nil :wk "Compile")
+   "cc" '(compile :wk "Compile")
+   "cv" '(graphviz-dot-view :wk "View")
+   "cp" '(graphviz-dot-preview :wk "Preview"))
   :custom
   (graphviz-dot-indent-width 2))
 
