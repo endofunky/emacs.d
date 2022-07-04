1e108a402ed02ed212c923c159fbe390cf4b9b0c
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-07-04 10:00:39 +0100

    Use display-buffer-alist rules from given rules plist

--- site-lisp/poe.el
+++ site-lisp/poe.el
@@ -1,11 +1,36 @@
 ;;; poe.el --- A popup manager -*- lexical-binding: t; -*-
 (require 'cl-extra)
+(require 'cl-seq)
 
 (defgroup poe nil
-  "A popup manager."
+  "A window and popup manager."
   :group 'convenience)
 
 (defvar poe-rules nil)
+
+(defvar poe-popup-default-rule
+  '(:side bottom
+    :size 0.2))
+
+(defun poe--plist-merge (&rest plists)
+  "Create a single property list from all plists in PLISTS.
+The process starts by copying the first list, and then setting
+properties from the other lists.  Settings in the last list
+are the most significant ones and overrule settings in the
+other lists.
+
+This is coming from `org-mode' (`org-combine-plists'). Requiring
+`org-mode' loads a 24k+ line Emacs Lisp file, which introduces
+significant overhead when used as a utility library, hence it
+has been extracted."
+  (let ((rtn (copy-sequence (pop plists)))
+        p v ls)
+    (while plists
+      (setq ls (pop plists))
+      (while ls
+        (setq p (pop ls) v (pop ls))
+        (setq rtn (plist-put rtn p v))))
+    rtn))
 
 (defun poe--match (buffer-or-name)
   (when-let* ((buffer (get-buffer buffer-or-name))
@@ -23,9 +48,18 @@
      return plist)))
 
 (defun poe--alist (alist rule)
-  '((side . bottom)
-    (reusable-frames . nil)
-    (size . 0.2)))
+  ;; If this is a popup, merge the default popup rules.
+  ;;
+  ;; Most of the time we want a panel of the bottom of the screen, so let's not
+  ;; require specifying those rules ever single time we're declaring a popup.
+  (let ((rule (if (plist-get rule :popupe)
+                  (poe--plist-merge poe-popup-default-rule
+                                    rule)
+                rule)))
+    `((actions         . ,(plist-get rule :actions))
+      (side            . ,(plist-get rule :side))
+      (size            . ,(plist-get rule :size))
+      (slot            . ,(plist-get rule :slot)))))
 
 (defun poe--display-buffer (buffer alist rule)
   (let ((alist (poe--alist alist rule))
@@ -44,6 +78,9 @@
     (when-let (window (cl-loop for func in actions
                                if (funcall func buffer alist)
                                return it))
+      (when (plist-get rule :popup)
+        ;; TODO set up the popup here
+        (message "popup"))
       window)))
 
 (defun poe--display-buffer-condition (buffer _action)
@@ -70,17 +107,23 @@
 
 (defmacro poe-rule (key &rest plist)
   (declare (indent 0))
-  `(push '(,key ,@plist) poe-rules))
+  `(progn
+     ;; Avoid having duplicate rules for a condition.
+     (setq poe-rules (cl-delete ,key poe-rules :key #'car :test #'equal))
+     (push '(,key ,@plist) poe-rules)))
 
 ;; ----------------------------------------------------------------------------
 ;; Modes
 ;; ----------------------------------------------------------------------------
 
 (defvar poe-popup-mode-map (make-sparse-keymap)
-  "Active keymap in `poe-mode' popup windows. See `poe-popup-mode'.")
+  "Active keymap in `poe-mode' popup windows.
+
+See `poe-popup-mode'.")
 
 (define-minor-mode poe-popup-mode
-  "Minor mode for individual `poe-mode' buffers to be shown in popup windows."
+  "Minor mode for individual `poe-mode' buffers to be shown in
+popup windows."
   :group 'poe
   :lighter nil
   :keymap poe-popup-mode-map)
