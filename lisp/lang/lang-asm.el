01f76fa51fea961a108e1a5f05f9141ecd4606fb
Tobias Svensson <tob@tobiassvensson.co.uk>
2022-09-01 08:53:46 +0100

    Load .nasm files with nasm-mode

--- lisp/lang/lang-asm.el
+++ lisp/lang/lang-asm.el
@@ -10,7 +10,7 @@
    "cc" '(compile :wk "Compile")))
 
 (use-package nasm-mode
-  :mode ("\\.asm\\'")
+  :mode ("\\.[n]asm\\'")
   :general
   (:prefix ef-local-leader :states 'normal :keymaps 'nasm-mode-map
    "c"  '(nil :wk "Compile")
