2e0224be23c667f7d0be225fac15604bbb63f7fa
Tobias Svensson <tob@tobiassvensson.co.uk>
2023-01-14 13:57:43 +0000

    Configure sql-connect to connect using postgres docker default password

--- lisp/lang/lang-sql.el
+++ lisp/lang/lang-sql.el
@@ -6,8 +6,12 @@
   :custom
   ;; PostgreSQL is the most common SQL database these days, so use that as the
   ;; default.
-  (sql-product 'postgres)
+  (sql-postgres-login-params nil)
+  (sql-connection-alist
+   '((psql-docker (sql-product 'postgres)
+                  (sql-database "postgres://postgres:postgres@localhost:5432/"))))
   :config
+  (poe-popup 'sql-interactive-mode :size 0.4 :select t)
   ;; Make double quotes work like a quoting character.
   ;;
   ;; See: https://github.com/Trevoke/sqlup-mode.el/issues/69
