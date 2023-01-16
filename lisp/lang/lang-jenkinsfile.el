69ccbba64e9142d238a98eb240d25e397288c38a
Tobias Svensson <tob@tobiassvensson.co.uk>
2023-01-16 08:53:55 +0000

    Add lang-jenkinsfile

---
+++ lisp/lang/lang-jenkinsfile.el
@@ -0,0 +1,7 @@
+;;; lang-go.el --- Jenkinsfile configuration -*- lexical-binding: t; -*-
+(require 'core-lib)
+
+(use-package jenkinsfile-mode
+  :mode "Jenkinsfile\\'")
+
+(provide 'lang-jenkinsfile)
