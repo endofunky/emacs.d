4184613f2d99d9f48260e7d66b6855b48b2a7f12
Tobias Svensson <tob@tobiassvensson.co.uk>
2023-08-07 14:59:13 +0100

    More face overrides ... this should probably be a custom theme

--- lisp/core/core-theme.el
+++ lisp/core/core-theme.el
@@ -13,7 +13,11 @@
   (doom-themes-org-config)
 
   (custom-set-faces
+   `(font-lock-builtin-face ((t :foreground "#7aa2f7")))
    `(poe-popup-dimmed-face ((t :background "#12121a")))
+   `(flymake-warning ((t :foreground "#f7768e" :underline nil)))
+   `(flymake-error ((t :foreground "#f7768e" :underline nil)))
+   `(flymake-note ((t :foreground "#f7768e" :underline nil)))
    `(hl-line ((t :background "#24283b")))))
 
 (use-package uniline
