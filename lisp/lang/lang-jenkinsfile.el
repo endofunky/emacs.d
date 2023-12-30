ac2381e6d5156d61ceb841d3209ceccba426d30c
Tobias Svensson <root@fuzzed.org>
2023-12-30 21:18:35 +0000

    Remove lang-jenkinsfile

--- lisp/lang/lang-jenkinsfile.el
+++
@@ -1,7 +0,0 @@
-;;; lang-go.el --- Jenkinsfile configuration -*- lexical-binding: t; -*-
-(require 'core-lib)
-
-(use-package jenkinsfile-mode
-  :mode "Jenkinsfile\\'")
-
-(provide 'lang-jenkinsfile)
