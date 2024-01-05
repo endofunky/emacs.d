d561318fb2954483058b8e2bbb2ca00de7efe018
Tobias Svensson <root@fuzzed.org>
2024-01-05 10:50:09 +0000

    Make org-mode block styling use regular background

--- lisp/core/core-theme.el
+++ lisp/core/core-theme.el
@@ -20,8 +20,23 @@
    `(font-lock-type-face ((t :foreground "#73daca")))
    `(hl-line ((t :background "#24283b")))
    `(nix-attribute-face ((t :foreground "#7dcfff")))
+   `(org-block ((t :background "#1a1b26")))
+   `(org-block-begin-line ((t :inherit nil :foreground "#343952" :background "#1a1b26" :extend t)))
+   `(org-block-end-line ((t :inherit nil :foreground "#343952" :background "#1a1b26" :extend t)))
+   `(org-block-background ((t :background "#1a1b26")))
    `(poe-popup-dimmed-face ((t :background "#12121a")))
-   `(vertico-current ((t :background "#24283b")))))
+   `(vertico-current ((t :background "#24283b"))))
+
+  (with-eval-after-load 'org
+    (set-face-attribute 'org-block-begin-line nil
+                        :box '(:line-width (-1 . 10)
+                               :color "#1a1b26"
+                               :style nil))
+
+    (set-face-attribute 'org-block-end-line nil
+                        :box '(:line-width (-1 . 10)
+                               :color "#1a1b26"
+                               :style nil))))
 
 (use-package uniline
   :demand t
